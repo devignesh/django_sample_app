65e7cdd1ac107a2837741377129308d61628ca08
devignesh <vinesh1865@gmail.com>
2018-08-21 23:41:09 +0530

    creating app

--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url
+
+from app import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^$', views.home, name='home'),
+    url('admin/', admin.site.urls),
 ]
